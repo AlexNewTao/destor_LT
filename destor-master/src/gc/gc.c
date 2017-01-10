43a7e4e4b208d18e8b19b9cb840fe504b04c5738
AlexNewTao <taoliu012@126.com>
2017-01-10 11:03:17 +0800

    change in gc alone

--- destor-master/src/gc/gc.c
+++ destor-master/src/gc/gc.c
@@ -16,13 +16,73 @@ time: 2016.11
 // 开始垃圾回收函数
 #include <stdio.h>
 #include <stdlib.h>
+//#include <glib.h>
+//#include "../utils/sds.h"
 #include "gc_rtm.h"
 #include "gc.h"
+#include "../destor.h"
+
+
+struct gc_list_type{
+	int64_t gc_containerid;
+	int32_t gc_chunk_shift;
+	struct gc_list_type *next;
+};
+
+
 
 
 static int64_t gc_count=0;
+void Destory_gc_list()
+{
+    struct gc_list_type *p = gchead;
+    while(gchead!=NULL)
+    {
+         p = gchead;
+         free(p);
+         gchead = gchead->next;
+    }
+}
 
-void gc_list_AddEnd (struct gc_list_data *gc_data)
+void gc_list_AddEnd(struct gc_list_type* gc_data)
+{
+
+    struct gc_list_type *node,*htemp;
+    printf("gc.c 54\n");
+    if (!(node=(struct gc_list_type *)malloc(sizeof(struct gc_list_type))))//分配空间
+    {
+        printf("fail alloc space!\n");	
+        return NULL;
+    }
+    else
+    {
+    	printf("gc.c 62\n");
+    	node->gc_containerid=gc_data->gc_containerid;//保存数据
+        node->gc_chunk_shift=gc_data->gc_chunk_shift;//保存数据
+        node->next=NULL;//设置结点指针为空，即为表尾；
+        printf("gc.c 64\n");
+        if (gchead==NULL)
+        {
+            gchead=node;
+          
+        }
+        else
+        {
+        	htemp=gchead;
+        	printf("gc.c73\n");
+        
+        	while(htemp->next!=NULL)
+        	{
+            	htemp=htemp->next;
+            	printf("1");
+        	}
+        	printf("gc.c 78\n");
+        	htemp->next=node;
+        }
+        printf("gc.c80\n");
+    }
+}
+/*void gc_list_AddEnd (struct gc_list_data *gc_data)
 {
     struct gc_list_type *node,*htemp;
     if (!(node=(struct gc_list_type *)malloc(sizeof(struct gc_list_type))))//分配空间
@@ -48,8 +108,7 @@ void gc_list_AddEnd (struct gc_list_data *gc_data)
         htemp->next=node;
         //return gchead;
     }
-}
-
+}*/
 
 void gc_reference_count()
 {
@@ -107,10 +166,12 @@ void gc_reference_time_map()
 	if (deletway==1)
 	{
 		gc_count=gc_reference_time_map_alone(deleteversion);
+	
 	}
 	else if (deletway==2)
 	{
 		gc_count=gc_reference_time_map_patch(deleteversion);
+
 	}
 	else
 	{
@@ -127,51 +188,292 @@ void get_delete_message()
 	printf("the collection  size is %ld MB\n", size);
 }
 
-//采用RTM方法进行垃圾回收
+/*//采用RTM方法进行垃圾回收
+int64_t gc_reference_time_map_alone(int deleteversion)
+{
+
+	get_real_reference_time_map();//得到实际的RTM
+	
+	show_RTM();
+
+	struct RTMdata *htemp;
+	htemp=RTMhead;
+
+	sds gcpath = sdsdup(destor.working_directory);
+    gcpath = sdscat(gcpath, "/gc_list.gc");
+
+    FILE *fp;
+    if((fp = fopen(gcpath, "a"))) 
+    {
+		while(htemp!=NULL)
+		{
+			int i=0;
+			//printf("gc111\n");
+			while(i<htemp->len)
+			{
+			//printf("gc444\n");
+				if (htemp->rtm[i]==deleteversion)
+				{
+        			int *s=(int32_t*)malloc(sizeof(int32_t));
+        			*s=i+1;
+        			fwrite(RTMhead->id, sizeof(int64_t), 1, fp);
+        			fwrite(s, sizeof(int32_t), 1, fp);
+					gc_count=gc_count+1;
+				}
+				i=i+1;
+			}
+		//printf("gc333\n");
+		htemp = htemp->next;
+		}
+	fclose(fp);
+	}
+
+	sdsfree(gcpath);
+	//printf("gc2222\n");
+	//根据RTM_check_list,检测RTM，得到实际回收的大小
+	return gc_count;
+
+	//get_delete_message();
+}*/
+
 int64_t gc_reference_time_map_alone(int deleteversion)
 {
 	//int *cbt;
 	//cbt=get_container_bit_table(deleteversion);//得到删除版本的container_bit_map
-	printf("1111\n");
+
 	get_real_reference_time_map();//得到实际的RTM
-	printf("2222\n");
-	while(RTMhead!=NULL)
+	
+	show_RTM();
+
+	struct RTMdata *htemp;
+	htemp = RTMhead;
+	while(htemp!=NULL)
 	{
 		int i=0;
-		while(i<RTMhead->len)
+			//printf("gc111\n");
+		while(i<htemp->len)
 		{
-			if (RTMhead->rtm[i++]==deleteversion)
+			printf("gc444\n");
+			if (htemp->rtm[i]==deleteversion)
 			{
-				struct gc_list_data *gc_data;
-				gc_data=(struct gc_list_data *)malloc(sizeof(struct gc_list_data));
-				gc_data->gc_containerid=RTMhead->id;
-				gc_data->gc_chunk_shift=i+1;
-				gc_list_AddEnd(gc_data);
-				gc_count++;
+				printf("5555, %d\n",sizeof(struct gc_list_type));
+				struct gc_list_type *gc_data=(struct gc_list_type *)malloc(sizeof(struct gc_list_type));
+				printf("66666");
+				gc_data->gc_containerid=0;//htemp->id;
+				printf("htemp->id is %d\n",htemp->id );
+				//gc_data->gc_chunk_shift=i+1;
+				gc_data->next = NULL;
+				
+				//gc_data = NULL;
+				//printf("2222\n");
+				//gc_list_AddEnd(gc_data); 
+				//free(gc_data);
+				//printf("11111111\n");			       		
+				gc_count=gc_count+1;
 			}
+			i=i+1;
 		}
-		RTMhead = RTMhead->next;
+		printf("gc333\n");
+		sleep(1);
+		htemp = htemp->next;
 	}
-	//根据RTM_check_list,检测RTM，得到实际回收的大小
-	return gc_count;
+	printf("4444\n");
 
+	Destory_gc_list();
+	
+	return gc_count;
 }
 
+/*int64_t gc_reference_time_map_alone(int deleteversion)
+{
+	//int *cbt;
+	//cbt=get_container_bit_table(deleteversion);//得到删除版本的container_bit_map
+
+	get_real_reference_time_map();//得到实际的RTM
+	
+	show_RTM();
+
+	int *check_arr=get_merge_container_bit_table(deleteversion);
+	GSequence *check_gsequence=g_sequence_new(free);
+	int n=get_container_bit_end();
+	int i;
+	for (i= 33; i <= n*2; i=i+2)
+	{
+		if (((get_CBT_array(i,check_arr)==1)||(get_CBT_array(i+1,check_arr)==1))
+		{
+			int *s=(int32_t*)malloc(sizeof(int32_t));
+			*s=(i-33)/2;
+            g_sequence_append(check_gsequence,s);
+		}
+	}
+
+	
+	struct RTMdata *htemp;
+	htemp = RTMhead;
+	int j=0;
+	while(htemp!=NULL)
+	{
+		GSequenceIter* iter=g_sequence_get_begin_iter(check_gsequence);
+    	GSequenceIter* end = g_sequence_get_end_iter(check_gsequence);
+    	while(iter!=end)
+    	{
+    		int *pos=g_sequence_get(iter);
+    		if (j==*pos)
+    		{
+    			break;
+    		}
+    		iter=g_sequence_iter_next(iter);
+    	}
+		if (iter!=end)
+		{
+			htemp = htemp->next;
+			j=j+1;
+		}
+		else
+		{
+			int i=0;
+			while(i<htemp->len)
+			{
+				if (htemp->rtm[i]==deleteversion)
+				{
+					struct gc_list_type *gc_data=(struct gc_list_type *)malloc(sizeof(struct gc_list_type));
+				
+					gc_data->gc_containerid=htemp->id;
+				
+					gc_data->gc_chunk_shift=i+1;
+				
+					gc_list_AddEnd(gc_data); 
+					gc_data = NULL;
+					free(gc_data);
+						       		
+					gc_count=gc_count+1;
+				}
+				i=i+1;
+			}
+			htemp = htemp->next;
+			j=j+1;
+		}
+		
+	}
 
 
+	Destory_gc_list();
+
+	g_sequence_free(check_gsequence);
+	return gc_count;
+}*/
+
+/*struct gc_list_type
+{
+	int64_t gc_containerid;
+	GSequence *gc_gsequence;
+	struct gc_list_type *next;
+};*/
+
+/*int64_t gc_reference_time_map_alone(int deleteversion)
+{
+	//int *cbt;
+	//cbt=get_container_bit_table(deleteversion);//得到删除版本的container_bit_map
+
+	get_real_reference_time_map();//得到实际的RTM
+	
+	show_RTM();
+
+	GSequence *gc_gsequence=g_sequence_new(free);
+
+	struct RTMdata *htemp;
+	htemp = RTMhead;
+	while(htemp!=NULL)
+	{
+		int i=0;
+		
+		while(i<htemp->len)
+		{
+			
+			if (htemp->rtm[i]==deleteversion)
+			{
+				
+				struct gc_list_type *gc_data=(struct gc_list_type *)malloc(sizeof(struct gc_list_type));
+				
+				gc_data->gc_containerid=htemp->id;
+				
+				gc_data->gc_chunk_shift=i+1;
+				
+				g_sequence_append(gc_gsequence,gc_data);
+				
+						       		
+				gc_count=gc_count+1;
+			}
+			i=i+1;
+		}
+		
+		
+		htemp = htemp->next;
+	}
+	
+
+	g_sequence_free(gc_gsequence);
+	
+	return gc_count;
+}
+*/
 
 
+/*int64_t gc_reference_time_map_alone(int deleteversion)
+{
+	//int *cbt;
+	//cbt=get_container_bit_table(deleteversion);//得到删除版本的container_bit_map
+
+	get_real_reference_time_map();//得到实际的RTM
+	
+	show_RTM();
+
+	GSequence *gc_gsequence=g_sequence_new(free);
+
+	struct RTMdata *htemp;
+	htemp = RTMhead;
+	while(htemp!=NULL)
+	{
+		int i=0;
+		printf("gc.c 297\n");
+		
+		while(i<htemp->len)
+		{
+			printf("gc.c 301\n");
+			if (htemp->rtm[i]==deleteversion)
+			{
+				printf("gc.c 304\n");
+				struct gc_list_type *gc_data=(struct gc_list_type *)malloc(sizeof(struct gc_list_type));
+				
+				gc_data->gc_containerid=htemp->id;
+				
+				gc_data->gc_chunk_shift=i+1;
+				printf("gc.c 310\n");
+				g_sequence_append(gc_gsequence,gc_data);
+				printf("gc.c 312\n");
+						       		
+				gc_count=gc_count+1;
+			}
+			i=i+1;
+		}
+		printf("gc333\n");
+		sleep(5);
+		htemp = htemp->next;
+	}
+	printf("4444\n");
+
+	g_sequence_free(gc_gsequence);
+	
+	return gc_count;
+}*/
 //批量回收
-int64_t gc_reference_time_map_patch(int deleteversion)
+
+int64_t  gc_reference_time_map_patch(int deleteversion)
 {
 	//int *cbt;
 	//cbt=get_container_bit_table(deleteversion);//得到删除版本的container_bit_map
 	
 	get_real_reference_time_map();//得到实际的RTM
-	
-	//gc_list_type *head1;
-
-	//struct RTMdata* rtm_head;
 
 	while(RTMhead!=NULL)
 	{
@@ -180,11 +482,12 @@ int64_t gc_reference_time_map_patch(int deleteversion)
 		{
 			if (RTMhead->rtm[i++]<=deleteversion)
 			{
-				struct gc_list_data *gc_data;
+				struct gc_list_type *gc_data;
+				gc_data=(struct gc_list_type *)malloc(sizeof(struct gc_list_type));
 				gc_data->gc_containerid=RTMhead->id;
-				gc_data->gc_chunk_shift=i;
+				gc_data->gc_chunk_shift=i+1;
 				gc_list_AddEnd(gc_data);
-				gc_count++;
+				gc_count=gc_count+1;
 			}
 		}
 		RTMhead = RTMhead->next;
@@ -192,6 +495,7 @@ int64_t gc_reference_time_map_patch(int deleteversion)
 	//根据RTM_check_list,检测RTM，得到实际回收的大小
 
 	return gc_count;
+	//get_delete_message();
 
 }
 
